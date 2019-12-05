6890c8aa9c6cdbb649a1b33f2a1cf1d613533380
Reena Shrestha <Reena@Indras-MacBook-Pro.local>
2019-12-04 23:54:07 -0600

    updates

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.static('public'))
-
-
+app.set('view engine','pug')
+app.get('/', (req, res) => {
+  res.render('index', {});
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
